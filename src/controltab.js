35ce513478fe2f00078b7ee48b4ce9a29223ab11
yapcheahshen <yapcheahshen@gmail.com>
2016-10-22 22:11:44 +0800

    set init page

--- src/controltab.js
+++ src/controltab.js
@@ -2,7 +2,7 @@ const React=require("react");
 const E=React.createElement;
 const ControlTab=React.createClass({
 	render:function(){
-		return E("div",{},"contrp=ols");
+		return E("div",{},"controls");
 	}
 })
 module.exports=ControlTab;
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 const React=require("react");
-const datapath=require("./datapath");
+const remotedata=require("./remotedata");
 const CorpusBindingMode=require("ksana2015-parallel").CorpusBindingMode;
 const E=React.createElement;
 const control=require("./controltab");
@@ -10,7 +10,9 @@ const maincomponent = React.createClass({
   ,render: function() {
     return E(CorpusBindingMode,
       {leftCorpus:"taisho",rightCorpus:"taixu",
-      appname:"taisho-taixu",datapath,control})
+      leftAddress:"1p1a1",rightAddress:"1p5a1",
+      leftFlex:3,rightFlex:5,
+      appname:"taisho-taixu",remotedata,control})
   }
 });
 module.exports=maincomponent;
